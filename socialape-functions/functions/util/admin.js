412727cf3ee6919231e86398f331674e55e23cb0
Machele-codez <smithbeblack@gmail.com>
2021-02-04 02:15:19 +0000

    normalized

--- socialape-functions/functions/util/admin.js
+++
@@ -1,9 +0,0 @@
-const admin = require("firebase-admin");
-
-// initialize firestore admin
-admin.initializeApp();
-
-// Firestore DB
-const db = admin.firestore();
-
-module.exports = { admin, db };
